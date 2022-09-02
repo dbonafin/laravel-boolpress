d3ab26528336852df8479cce1aab7c911c9a2d69
dbonafin <demianbonafina@gmail.com>
2022-09-02 15:54:02 +0200

    admin home controller done

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// If the user is logged, show the admin pages
+Route::middleware('auth')
+->name('admin.')
+->namespace('Admin')
+->prefix('admin')
+->group(function() {
+    Route::get('/', 'HomeController@index')->name('home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// If the user is not logged, show the guest pages
+Route::get('{any?}', function () {
+    return view('welcome');
+});
+
