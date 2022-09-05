fce7c984c7963afb8505719ea3e3389f1d8cb0a5
dbonafin <demianbonafina@gmail.com>
2022-09-05 16:56:50 +0200

    show post details done

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,18 +4,19 @@
 
     <h2>Posts feed</h2>
 
-   
-
-   <div class="row">
+    <div class="row">
         @foreach ($posts as $post)
         
             <div class="col-sm-4 mt-2">
-
                 <div class="card">
+                    
                     <div class="card-body">
                         <h3 class="card-title"> {{$post->title}} </h3>
 
-                        <a href="#" class="btn btn-primary">Details</a>
+                        <a href="{{route('admin.posts.show', ['post' => $post->id])}}" 
+                            class="btn btn-primary">
+                            Details
+                        </a>
                     </div>
 
                 </div>
---
+++ resources/views/admin/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+    <div class="card text-center">
+
+        <div class="card-body">
+          <h2 class="card-title"> {{$post->title}} </h2>
+
+          <p class="card-text"> {{$post->content}} </p>
+
+          <a href="#" class="btn btn-primary">Edit</a>
+          <a href="#" class="btn btn-danger">Delete</a>
+        </div>
+
+        <div class="card-footer text-muted">
+
+          <div> Created: {{$post->created_at}} </div>
+
+          <div> Last update: {{$post->created_at}} </div>
+
+        </div>
+        
+    </div>
+@endsection
