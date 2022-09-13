a31a5e55342a19e14120f974dee410310c1c7415
dbonafin <demianbonafina@gmail.com>
2022-09-13 14:48:59 +0200

    vue router added

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,8 +31,10 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import App from "./views/App.vue";
+import router from "./router.vue";
 
 const app = new Vue({
     el: '#root',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
